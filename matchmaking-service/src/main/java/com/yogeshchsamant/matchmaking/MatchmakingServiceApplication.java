3304d0b8e5318061477a1cab912c5a6b98608abe
yogesh-ch-ss <yogeshchsamant@gmail.com>
2025-07-02 16:48:16 +0100

    matchmaking-service init

---
+++ matchmaking-service/src/main/java/com/yogeshchsamant/matchmaking/MatchmakingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yogeshchsamant.matchmaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchmakingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchmakingServiceApplication.class, args);
+	}
+
+}
---
+++ matchmaking-service/src/test/java/com/yogeshchsamant/matchmaking/MatchmakingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogeshchsamant.matchmaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchmakingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
