8dbd5fb371f531cb8bd6c85de8d38596692f99ec
yogesh-ch-ss <yogeshchsamant@gmail.com>
2025-07-17 15:31:01 +0100

    gameplay-service init

---
+++ gameplay-service/src/test/java/com/yogeshchsamant/gameplay/GameplayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogeshchsamant.gameplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameplayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
