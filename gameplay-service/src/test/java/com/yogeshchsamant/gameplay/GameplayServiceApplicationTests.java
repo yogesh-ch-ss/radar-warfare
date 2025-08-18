471e83553b2e6d4c2026cf0fdc70f2fe0b51107b
yogesh-ch-ss <yogeshchsamant@gmail.com>
2025-08-18 23:38:43 +0100

    render.yaml for deployment to render

--- gameplay-service/src/test/java/com/yogeshchsamant/gameplay/GameplayServiceApplicationTests.java
+++ gameplay-service/src/test/java/com/yogeshchsamant/gameplay/GameplayServiceApplicationTests.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
